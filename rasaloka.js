e6cd9d14486918e10f7122a7136733924b18dd47
erteooo <raditoktaviano24@gmail.com>
2024-10-23 05:08:34 +0700

    update 23102024=05:08

--- rasaloka.js
+++ rasaloka.js
@@ -39,3 +39,44 @@ document.addEventListener("DOMContentLoaded", function() {
         hidePopup();
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const cartIcon = document.querySelector(".rasaloka-cart");
+    const cartPopup = document.getElementById('cart-popup');
+    let isMouseInCartOrPopup = false;
+
+    // Fungsi untuk menampilkan popup
+    const showCartPopup = () => {
+        cartPopup.classList.add('active');
+    };
+
+    // Fungsi untuk menyembunyikan popup
+    const hideCartPopup = () => {
+        cartPopup.classList.remove('active');
+    };
+
+    // Event ketika mouse berada di atas ikon keranjang
+    cartIcon.addEventListener("mouseenter", function() {
+        showCartPopup();
+    });
+
+    // Event ketika mouse meninggalkan ikon keranjang
+    cartIcon.addEventListener("mouseleave", function() {
+        setTimeout(() => {
+            if (!isMouseInCartOrPopup) {
+                hideCartPopup();
+            }
+        }, 200);
+    });
+
+    // Event ketika mouse berada di atas popup keranjang
+    cartPopup.addEventListener("mouseenter", function() {
+        isMouseInCartOrPopup = true;
+    });
+
+    // Event ketika mouse meninggalkan popup keranjang
+    cartPopup.addEventListener("mouseleave", function() {
+        isMouseInCartOrPopup = false;
+        hideCartPopup();
+    });
+});
